3b1e2a0a9dc5d77c9d344446cae9554efdc2f3d1
Eduard <mamaxropela@gmail.com>
2014-11-06 22:08:49 +0200

    add overloaded ContinueWith function

---
+++ TasksLibrary/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TasksLibrary
+{
+	public static class Extensions
+	{
+		public static Task ContinueWith(this Task task, Action action)
+		{ 
+			return task.ContinueWith(a => action(), TaskScheduler.FromCurrentSynchronizationContext());
+		}
+		public static Task ContinueWith(this Task task, Action<Task> action)
+		{
+			return task.ContinueWith(action, TaskScheduler.FromCurrentSynchronizationContext());
+		}
+		public static async Task<TResult> ContinueWith<TResult>(this Task task, Func<TResult> func)
+		{
+			return await task.ContinueWith(ignored => func(), TaskScheduler.FromCurrentSynchronizationContext());
+		}
+		public async static Task<TResult> ContinueWith<TResult>(this Task task, Func<Task<TResult>> func)
+		{
+			return await await task.ContinueWith(ignored => func(), TaskScheduler.FromCurrentSynchronizationContext());
+		}
+
+		public static async Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, TResult> func)
+		{
+			return await task.ContinueWith(func, TaskScheduler.FromCurrentSynchronizationContext());
+		}
+
+		public static async Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, Task<TResult>> func)
+		{
+			return await await task.ContinueWith(func, TaskScheduler.FromCurrentSynchronizationContext());
+		}
+	}
+}
