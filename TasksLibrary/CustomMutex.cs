7bc4b2eb7c47b219e24dc3355c82a491e59d1549
Eduard <mamaxropela@gmail.com>
2014-11-07 19:52:24 +0200

    complete task4 (using keyword for mutex)

--- TasksLibrary/CustomMutex.cs
+++ TasksLibrary/CustomMutex.cs
@@ -9,6 +9,7 @@ namespace TasksLibrary
 {
 	public class CustomMutex
 	{
+		private readonly Task<Releaser> releaser;
 		private static readonly Task complete_task = Task.FromResult(true);
 		private readonly Queue<TaskCompletionSource<bool>> waiters = new Queue<TaskCompletionSource<bool>>();
 		private bool _isFree;
@@ -16,6 +17,17 @@ namespace TasksLibrary
 		public CustomMutex()
 		{
 			_isFree = true;
+			releaser = Task.FromResult(new Releaser(this));
+		}
+
+		public Task<Releaser> LockSection()
+		{
+			var wait = Lock();
+			return wait.IsCompleted ?
+				releaser :
+				wait.ContinueWith((_, state) => new Releaser((CustomMutex) state),
+					this, CancellationToken.None,
+					TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default); 
 		}
 		public Task Lock()
 		{
@@ -46,6 +58,21 @@ namespace TasksLibrary
 			}
 			if (toRelease != null)
 				toRelease.SetResult(true);
+		}
+
+		public struct Releaser : IDisposable
+		{
+			private readonly CustomMutex mutex;
+
+			internal Releaser(CustomMutex mutex)
+			{
+				this.mutex = mutex;
+			}
+			public void Dispose()
+			{
+				if(mutex!=null)
+					mutex.Release();
+			}
 		}
 	}
 }
