03fe8a1db3d94635e546058495af8680baad3a88
Eduard <mamaxropela@gmail.com>
2014-11-09 20:31:39 +0200

    delete using directives

--- TasksLibrary/RandomGenerator.cs
+++ TasksLibrary/RandomGenerator.cs
@@ -1,12 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Runtime.Serialization.Json;
-using System.Text;
 using System.Threading.Tasks;
-using System.Runtime.Serialization;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
