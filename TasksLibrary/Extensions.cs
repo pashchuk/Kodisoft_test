5e960701766a53d89a8adeec4ea642f0c5fd3331
Eduard <mamaxropela@gmail.com>
2014-11-09 20:29:37 +0200

    rename methods

--- TasksLibrary/Extensions.cs
+++ TasksLibrary/Extensions.cs
@@ -1,37 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
+using System.Windows;
 
 namespace TasksLibrary
 {
 	public static class Extensions
 	{
-		public static Task ContinueWith(this Task task, Action action)
+		public static Task CustomContinueWith(this Task task, Action action)
 		{ 
 			return task.ContinueWith(a => action(), TaskScheduler.FromCurrentSynchronizationContext());
 		}
-		public static Task ContinueWith(this Task task, Action<Task> action)
+		public static Task CustomContinueWith(this Task task, Action<Task> action)
 		{
 			return task.ContinueWith(action, TaskScheduler.FromCurrentSynchronizationContext());
 		}
-		public static async Task<TResult> ContinueWith<TResult>(this Task task, Func<TResult> func)
+		public static async Task<TResult> CustomContinueWith<TResult>(this Task task, Func<TResult> func)
 		{
 			return await task.ContinueWith(ignored => func(), TaskScheduler.FromCurrentSynchronizationContext());
 		}
-		public async static Task<TResult> ContinueWith<TResult>(this Task task, Func<Task<TResult>> func)
+		public async static Task<TResult> CustomContinueWith<TResult>(this Task task, Func<Task<TResult>> func)
 		{
 			return await await task.ContinueWith(ignored => func(), TaskScheduler.FromCurrentSynchronizationContext());
 		}
 
-		public static async Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, TResult> func)
+		public static async Task<TResult> CustomContinueWith<TResult>(this Task task, Func<Task, TResult> func)
 		{
 			return await task.ContinueWith(func, TaskScheduler.FromCurrentSynchronizationContext());
 		}
 
-		public static async Task<TResult> ContinueWith<TResult>(this Task task, Func<Task, Task<TResult>> func)
+		public static async Task<TResult> CustomContinueWith<TResult>(this Task task, Func<Task, Task<TResult>> func)
 		{
 			return await await task.ContinueWith(func, TaskScheduler.FromCurrentSynchronizationContext());
 		}
