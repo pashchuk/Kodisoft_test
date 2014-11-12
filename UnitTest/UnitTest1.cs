3f9f40c074067e5327eada83f3ba5f77a5f5022c
Eduard <mamaxropela@gmail.com>
2014-11-12 23:08:03 +0200

    create unit tests for clone methods

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.CodeDom;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PluginLibrary.Plugins;
 using TasksLibrary;
 
 namespace UnitTest
@@ -93,6 +96,109 @@ namespace UnitTest
 			Assert.IsTrue(task2.IsCompleted);
 			await task2;
 			mutex.Release();
+		}
+
+		[TestMethod]
+		public void Test_CloneString()
+		{
+			//arrange
+			var str = "test test";
+
+			//act
+			var clone = str.DeepClone();
+
+			//assert
+			Assert.AreEqual(str, clone);
+			Assert.AreNotSame(str, clone);
+		}
+
+		[TestMethod]
+		public void Test_CloneValueType()
+		{
+			//arrange
+			int a = 10;
+			double b = 20.357;
+			var c = BindingFlags.NonPublic;
+
+			//act
+			var a_clone = a.DeepClone();
+			var b_clone = b.DeepClone();
+			var c_clone = c.DeepClone();
+
+			//assert
+			Assert.AreEqual(a_clone, a);
+			Assert.AreEqual(b_clone, b);
+			Assert.AreEqual(c_clone, c);
+			Assert.AreNotSame(a_clone, a);
+			Assert.AreNotSame(b_clone, b);
+			Assert.AreNotSame(c_clone, c);
+		}
+
+		[TestMethod]
+		public void Test_CloneRef()
+		{
+			//arrange
+			var plugin = new IntPlugin(10);
+			var result = plugin.Modify(2);
+
+			//act
+			var clone = plugin.DeepClone();
+			var clone_res = clone.Modify(2);
+
+			//assert
+			Assert.AreNotSame(plugin, clone);
+			Assert.AreEqual(result, clone_res);
+		}
+
+		[TestMethod]
+		public void Test_ILCloneRef()
+		{
+			//arrange
+			var a = new IntPlugin(10);
+			var res = a.Modify(2);
+
+			//act
+			var clone = a.ILClone();
+			var clone_res = clone.Modify(2);
+
+			//assert
+			Assert.AreNotSame(a, clone);
+			Assert.AreEqual(res, clone_res);
+		}
+
+		[TestMethod]
+		public void Test_ILCloneValueType()
+		{
+			//arrange
+			int a = 10;
+			double b = 20.357;
+			var c = BindingFlags.NonPublic;
+
+			//act
+			var a_clone = a.ILClone();
+			var b_clone = b.ILClone();
+			var c_clone = c.ILClone();
+
+			//assert
+			Assert.AreEqual(a_clone, a);
+			Assert.AreEqual(b_clone, b);
+			Assert.AreEqual(c_clone, c);
+			Assert.AreNotSame(a_clone, a);
+			Assert.AreNotSame(b_clone, b);
+			Assert.AreNotSame(c_clone, c);
+		}
+		[TestMethod]
+		public void Test_ILCloneString()
+		{
+			//arrange
+			var str = "test test";
+
+			//act
+			var clone = str.ILClone();
+
+			//assert
+			Assert.AreEqual(str, clone);
+			Assert.AreNotSame(str, clone);
 		}
 	}
 }
