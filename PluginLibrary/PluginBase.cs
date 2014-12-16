0d67dd02f0bdc3fae76148da3d87ddbd0aee4f1d
Eduard <mamaxropela@gmail.com>
2014-12-16 18:22:28 +0200

    change generic parameters from (T, U) to (T)

--- PluginLibrary/PluginBase.cs
+++ PluginLibrary/PluginBase.cs
@@ -2,14 +2,14 @@
 
 namespace PluginLibrary
 {
-	public abstract class PluginBase<T, U> where T:Plugin<U>
+	public abstract class PluginBase<T> where T : IPlugin
 	{
 		public T Plugin { get; set; }
-		public U Data { get; set; }
+		public object Data { get; set; }
 
 		public virtual void Print()
 		{
-			Console.WriteLine("Before applying plugin: {0}",Plugin.GetType().Name);
+			Console.WriteLine("Before applying plugin: {0}", Plugin.GetType().Name);
 			Console.WriteLine(Data.ToString());
 			Data = Plugin.Modify(Data);
 			Console.WriteLine("After applying plugin: {0}", Plugin.GetType().Name);
